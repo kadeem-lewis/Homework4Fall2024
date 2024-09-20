825a3b0c3d0e536e4d1ee4bc11e841247d1e4b36
kadeem-lewis <email@kadeemlewis.com>
2024-09-20 03:25:58 -0400

    updated operations to include typing and to use fractions for better precision

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,12 +1,22 @@
-def add(a,b):
-    return a + b
+"Main calculator code"
+from fractions import Fraction
 
-def subtract(a,b):
-    return a - b
 
-def multiply(a,b):
-    return a * b
+def add(addend_one: Fraction, addend_two: Fraction) -> Fraction:
+    """Add two numbers"""
+    return addend_one + addend_two
 
-def divide(a,b):
-    return a / b
 
+def subtract(minuend: Fraction, subtrahend: Fraction) -> Fraction:
+    """Subtract two numbers"""
+    return minuend - subtrahend
+
+
+def multiply(multiplier: Fraction, multiplicand: Fraction) -> Fraction:
+    """Multiply two numbers"""
+    return multiplier * multiplicand
+
+
+def divide(dividend: Fraction, divisor: Fraction) -> Fraction:
+    """Divide two numbers"""
+    return dividend / divisor
