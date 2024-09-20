2fb54ccc897ca11e5d6c7fa9113c99f4d17bfc50
kadeem-lewis <email@kadeemlewis.com>
2024-09-20 00:33:11 -0400

    created tests for other basic calculator operations

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,5 +1,5 @@
 '''My Calculator Test'''
-from calculator import add, subtract
+from calculator import add, subtract, divide, multiply
 
 def test_addition():
     '''Test that addition function works '''    
@@ -9,3 +9,11 @@ def test_subtraction():
     '''Test that addition function works '''    
     assert subtract(2,2) == 0
     
+def test_division():
+    '''Test that addition function works '''    
+    assert divide(2,2) == 1
+
+def test_multiplication():
+    '''Test that addition function works '''    
+    assert multiply(2,2) == 4
+
