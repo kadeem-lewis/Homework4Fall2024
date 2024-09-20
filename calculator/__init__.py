cc2406d245960ceb3c7176e6636da546c383c35a
kadeem-lewis <email@kadeemlewis.com>
2024-09-20 05:25:31 -0400

    created basic calculator class that returns the different operations

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -20,3 +20,34 @@ def multiply(multiplier: Fraction, multiplicand: Fraction) -> Fraction:
 def divide(dividend: Fraction, divisor: Fraction) -> Fraction:
     """Divide two numbers"""
     return dividend / divisor
+
+
+class Calculator:
+    """Class responsible for the calculator operations"""
+
+    @staticmethod
+    def add(var_one: Fraction, var_two: Fraction) -> Fraction:
+        """Add two numbers"""
+        return add(var_one, var_two)
+
+    @staticmethod
+    def subtract(minuend: Fraction, subtrahend: Fraction) -> Fraction:
+        """Subtract two numbers"""
+        return subtract(minuend, subtrahend)
+
+    @staticmethod
+    def multiply(multiplier: Fraction, multiplicand: Fraction) -> Fraction:
+        """Multiply two numbers"""
+        return multiply(multiplier, multiplicand)
+
+    @staticmethod
+    def divide(dividend: Fraction, divisor: Fraction) -> Fraction:
+        """Divide two numbers"""
+        return divide(dividend, divisor)
+
+
+# Calculator is high level so it will need to have the operations in them so you can call Calculator.add() etc
+# Each operation will call the static factory method in the Calculation class along with one of the operations
+# The Calculation class needs to store the variables and operation because it needs to get added to history
+# Operations stored the basic functions for arithmetic operations
+# Calculation will
