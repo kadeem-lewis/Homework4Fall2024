0d6917ba9e0ccd59b7e526f6a6ab2e1fa1880c5b
kadeem-lewis <email@kadeemlewis.com>
2024-09-20 03:27:01 -0400

    updated tests to match use of fractions for operations

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,19 +1,24 @@
-'''My Calculator Test'''
+"""My Calculator Test"""
+
+from fractions import Fraction
 from calculator import add, subtract, divide, multiply
 
+
 def test_addition():
-    '''Test that addition function works '''    
-    assert add(2,2) == 4
+    """Test that addition function works"""
+    assert add(Fraction(2), Fraction(2)) == 4
+
 
 def test_subtraction():
-    '''Test that addition function works '''    
-    assert subtract(2,2) == 0
-    
+    """Test that addition function works"""
+    assert subtract(Fraction(2), Fraction(2)) == 0
+
+
 def test_division():
-    '''Test that addition function works '''    
-    assert divide(2,2) == 1
+    """Test that addition function works"""
+    assert divide(Fraction(2), Fraction(2)) == 1
+
 
 def test_multiplication():
-    '''Test that addition function works '''    
-    assert multiply(2,2) == 4
-
+    """Test that addition function works"""
+    assert multiply(Fraction(2), Fraction(2)) == 4
