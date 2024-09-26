dde3423f4b568da26047cf8909223419332e2363
kadeem-lewis <email@kadeemlewis.com>
2024-09-26 12:07:41 -0400

    added tests for calculator methods

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,31 +1,24 @@
-"""My Calculator Test"""
+"""Testing Calculator"""
 
 from fractions import Fraction
-import pytest
-from calculator import add, subtract, divide, multiply
+from calculator import Calculator
 
 
 def test_addition():
     """Test that addition function works"""
-    assert add(Fraction(2), Fraction(2)) == 4
+    assert Calculator.add(Fraction(2), Fraction(2)) == Fraction(4)
 
 
 def test_subtraction():
-    """Test that addition function works"""
-    assert subtract(Fraction(2), Fraction(2)) == 0
+    """Test that subtraction function works"""
+    assert Calculator.subtract(Fraction(2), Fraction(2)) == Fraction(0)
 
 
 def test_division():
-    """Test that addition function works"""
-    assert divide(Fraction(2), Fraction(2)) == 1
+    """Test that division function works"""
+    assert Calculator.divide(Fraction(2), Fraction(2)) == Fraction(1)
 
 
 def test_multiplication():
-    """Test that addition function works"""
-    assert multiply(Fraction(2), Fraction(2)) == 4
-
-
-def test_divide_by_zero():
-    """Test that division by zero raises an error"""
-    with pytest.raises(ZeroDivisionError, match="Cannot divide by zero"):
-        divide(Fraction(2), Fraction(0))
+    """Test that multiplication function works"""
+    assert Calculator.multiply(Fraction(2), Fraction(2)) == Fraction(4)
