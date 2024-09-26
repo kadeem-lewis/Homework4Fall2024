b01b9840a21ddd1cc9ce173c1fc6be5037fa1a3a
kadeem-lewis <email@kadeemlewis.com>
2024-09-26 12:22:24 -0400

    updated history to make history list private and to have better type hinting

--- calculator/history.py
+++ calculator/history.py
@@ -1,36 +1,36 @@
 """Module to hold the history of all calculations performed by the calculator."""
 
-from typing import List
+from typing import List, Optional
 from calculator.calculation import Calculation
 
 
 class History:
     """Class to hold the history of all calculations performed by the calculator."""
 
-    history: List[Calculation] = []
+    _history: List[Calculation] = []
 
     @classmethod
     def get_history(cls) -> List[Calculation]:
         """Return the history of all calculations."""
-        return cls.history
+        return cls._history
 
     @classmethod
     def set_history(cls, history: List[Calculation]):
         """Set the history of all calculations."""
-        cls.history.clear()
-        cls.history.extend(history)
+        cls._history.clear()
+        cls._history.extend(history)
 
     @classmethod
     def add_calculation(cls, calculation: Calculation):
         """Add a calculation to the history."""
-        cls.history.append(calculation)
+        cls._history.append(calculation)
 
     @classmethod
     def clear_history(cls):
         """Clear the history of all calculations."""
-        cls.history.clear()
+        cls._history.clear()
 
     @classmethod
-    def get_last_calculation(cls) -> Calculation | None:
+    def get_last_calculation(cls) -> Optional[Calculation]:
         """Return the last calculation performed."""
-        return cls.history[-1] if cls.history else None
+        return cls._history[-1] if cls._history else None
